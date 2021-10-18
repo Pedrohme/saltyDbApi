2dff19e39cfa538608bd66d6580b34a13e6d89f5
Pedrohme <pedrohmen45@gmail.com>
2021-10-18 13:39:13 -0300

    fix: added jwt verification for index POST

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import path from "path";
 import api from "./api";
 import fighter from "./fighter";
 import indexController from "../controllers/indexController";
+import loginController from "../controllers/api/login";
 const router = express.Router();
 
 router.use('/', api.router);
@@ -12,7 +13,7 @@ router.use('/scripts', express.static(path.join(__dirname, "../views/js")));
 router.use('/styles', express.static(path.join(__dirname, "../views/css")));
 
 router.get('/', indexController.renderIndexPage);
-router.post('/', indexController.updateFighters);
+router.post('/', loginController.verifyJWT, indexController.updateFighters);
 
 
 export default { router };
